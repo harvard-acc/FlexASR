b91cb433fef72dee46bb57ba3921239ec36de261
Sawyer Lai <sawyer.s.lai@gmail.com>
2026-02-24 11:28:55 -0800

    fixing PEcore & cmod_Makefile

--- cmod/include/PECoreSpec.h
+++ cmod/include/PECoreSpec.h
@@ -33,7 +33,7 @@ namespace spec {
       const int kNumReadPorts = kNumVectorLanes; // spec::kNumVectorLanes = 16
       const int kNumWritePorts = 1;
       const int kNumBanks = kNumVectorLanes;
-      const int kEntriesPerBank = 4096;       // need to configure
+      const int kEntriesPerBank = 448;       // configured to 448 to get entire PE memory to < 128KB
       const unsigned int kAddressWidth = nvhls::index_width<kNumBanks * kEntriesPerBank>::val;
       const unsigned int kBankIndexSize = nvhls::index_width<kNumBanks>::val;
       const unsigned int kLocalIndexSize = nvhls::index_width<kEntriesPerBank>::val;
